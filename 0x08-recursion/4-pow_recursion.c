1795b278af25d7c49282e2d5692106bc512722b5
onepurp <on3purp@gmail.com>
2023-06-08 19:53:12 -0700

    Add 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,21 +4,22 @@
  * _pow_recursion- @
  * @x: #
  * @y: #
- * Return: -1 if x, y < 0 & 1 if x == 0 & x ^ y if x > 0
+ * Return: -1 if y < 0 & 1 if y == 0
+ * & _pow(x * factorial(x, y - 1)) if y > 0
  */
 
 int _pow_recursion(int x, int y)
 {
-	if (x < 0 && y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-	else if (x == 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return (x ^ y);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }

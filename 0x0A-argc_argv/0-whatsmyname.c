3f3fe3a60d24380c9f9ad62becd6b4e27e117a27
onepurp <on3purp@gmail.com>
2023-06-09 14:13:26 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main- #
+ * @argc: #
+ * @argv: #
+ * Return: 0
  */
 
-int main (int argc __attribute__((unused)), char **argv)
+int main(int argc__attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);

5e7b9ecbe4e21e182a4477a5633be33edf9b6dfa
onepurp <on3purp@gmail.com>
2023-06-09 17:55:31 -0700

    Add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- #
+ * @argc: @
+ * @argv: @
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x, r = 1;
+
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			r = r * atoi(argv[x]);
+		}
+		printf("%d\n", r);
+	}
+	else if (argc == 1)
+	{
+		printf("Eror\n");
+		return (1);
+	}
+	return (0);
+}

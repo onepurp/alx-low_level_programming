06e7b7517f77854b092a19968d7211910ddf0018
onepurp <on3purp@gmail.com>
2023-06-20 01:38:00 -0700

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,11 @@
  * @age: #
  */
 
-struct dog
+typedef struct dog
 {
 	char *name, *owner;
 	float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
